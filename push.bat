91e9a722d7a8c6b45266a58ae48c767ba1682751
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2021-12-05 02:42:46 +0100

    Feat/V2.1/#30000/

--- push.bat
+++ push.bat
@@ -9,16 +9,20 @@ set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH=
 set /P BRANCH=BRANCH: %=%
 %GIT_PATH% status
+echo would you add this change in branch (y):Yes/(n):No 
+set AddACTION=
+set /P AddACTION=Add: %=%
+if "%AddACTION%"=="y" (
 %GIT_PATH% add -A
 %GIT_PATH% status
 echo type Nom de BRANCH
-cd "curl"
+
 set MessageCommit=
 set /P MessageCommit=Message de Commit: %=%
-set commit=%BRANCH%/%MessageCommit%
-%GIT_PATH% commit -am "%commit%"
+%GIT_PATH% commit -am "%BRANCH%/%MessageCommit%"
 
 %GIT_PATH% push -u origin %BRANCH%
 echo "%BRANCH%"
 if "%BRANCH%"=="exit" exit /b
 pause
+)
