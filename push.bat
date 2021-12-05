38c5048ebe0d70f3a60425fdc95435cdb7bb962c
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2021-12-05 01:51:37 +0100

    Feat/V2.1/#30000/New Feature

--- push.bat
+++ push.bat
@@ -6,11 +6,11 @@ set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin"
 
 :P
-set ACTION=
-set /P ACTION=Action: %=%
+set CommitMessage=
+set /P CommitMessage=Message de Commit: %=%
 %GIT_PATH% add -A
-%GIT_PATH% commit -am "%ACTION%"
+%GIT_PATH% commit -am "%CommitMessage%"
 
 %GIT_PATH% push %BRANCH%
 echo "%BRANCH%"
-if "%ACTION%"=="exit" exit /b
+if "%CommitMessage%"=="exit" exit /b
